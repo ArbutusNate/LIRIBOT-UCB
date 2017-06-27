3fd508f1f3d717cc5b95570317f7e84a8be19dc3
ArbutusNate <arbutusnate@gmail.com>
2017-06-27 13:01:09 -0700

    Added pack,json dependencies, added twitter keys to keys.js, added backstreet boys to random.txt

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'KePwN2OapkWx9FTltEYDeXmCu',
+  consumer_secret: 'hZUkZpYkP3XlOvmkgd2Wop8tK83x1Fdc668fai1oXvYBDGwIWZ',
+  access_token_key: '854528545984462848-09YrNVC0oRhpvL78s54ph1Yjnf7Jgsh',
+  access_token_secret: 'nZCZPR2TaAm9lOj4CKWARk6nsrFCH2nDovhGi3w6KW0Mz',
 }
